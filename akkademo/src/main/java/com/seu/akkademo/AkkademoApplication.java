7d41250821d1944cff98ae60f237ebecdca46cf5
dengdonghz <dengdonghz@cmhi.chinamobile.com>
2019-11-11 16:13:30 +0800

    Initial project

---
+++ akkademo/src/main/java/com/seu/akkademo/AkkademoApplication.java
@@ -0,0 +1,13 @@
+package com.seu.akkademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkkademoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkkademoApplication.class, args);
+    }
+
+}
